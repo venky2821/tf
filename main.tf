72d968d67dd7e01a2822e997c276ba8a84f6b8e2
venkatesh <venkatesh.seenu@valpo.edu>
2024-02-01 05:00:32 +0000

    edited main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 provider "azurerm"  {
   features {}
-}
 
-resource "azurerm_virtual_network" "net" {
+resource "azurerm_resource_group" "rg" {
+ name = "venkatesh-rg"
+ location = "Central US"
+ }   
+}
+resource "azurerm_virtual_network" "vnet" {
  name = "myvnet"
  location = "Central US"
  address_space = ["10.0.0.0/16"]
