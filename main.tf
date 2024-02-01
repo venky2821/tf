af094ffd59ce17216126a741a905ee37f3f3f886
venky2821 <158250111+venky2821@users.noreply.github.com>
2024-02-01 01:26:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "azurerm"  {
     features {}
 }
-  resource "azurerm_resource_group" "rg" {
-   name = "venkatesh-rg"
-   location = "Central US"
-   }   
+ // resource "azurerm_resource_group" "rg" {
+  // name = "venkatesh-rg"
+  // location = "Central US"
+    
   
   resource "azurerm_virtual_network" "vnet" {
    name = "myvnet"
@@ -15,13 +15,13 @@ provider "azurerm"  {
   resource "azurerm_subnet" "subnet" {
     name = "mysubnet"
     virtual_network_name = azurerm_virtual_network.vnet.name
-    resource_group_name = azurerm_resource_group.rg.name
+    resource_group_name = "venkatesh-rg"
     address_prefixes =   ["10.0.10.0/24"]
   }
   resource "azurerm_network_interface" "nic" {
     name = "my-nic"
     location = "Central US"
-    resource_group_name = azurerm_resource_group.rg.name
+    resource_group_name = "venkatesh-rg"
     ip_configuration {
        name = "myipconfig"
        subnet_id = azurerm_subnet.subnet.id
@@ -32,21 +32,21 @@ provider "azurerm"  {
   resource "azurerm_public_ip" "pip" {
     name = "venkyip"
     location = "Central US"
-    resource_group_name = "${azurerm_resource_group.rg.name}"
+    resource_group_name = "${venkatesh-rg}"
     allocation_method = "Dynamic"
     domain_name_label = "venky"
   }
   resource "azurerm_storage_account" "stor" {
     name = "venky"
     location = "Central US"
-    resource_group_name = azurerm_resource_group.rg.name
+    resource_group_name = "venkatesh-rg"
     account_tier = "Standard"
     account_replication_type = "LRS"
   }
   resource "azurerm_linux_virtual_machine" "vm" {
     name = "LinuxVm"
     location = "Central US"
-    resource_group_name = azurerm_resource_group.rg.name
+    resource_group_name = "venkatesh-rg"
     network_interface_ids = ["${azurerm_network_interface.nic.id}"]
     size = "Standard_DS1_v2"
    
@@ -55,7 +55,11 @@ provider "azurerm"  {
       caching              = "ReadWrite"
       storage_account_type = "Premium_LRS"
     }
-    admin_username = "var.username"
+    admin_username = "azureadmin"
+   admin_ssh_key {
+    username = "azureadmin"
+    public_key = "file(/.ssh/venky.pub)"
+  }
   
     source_image_reference {
         publisher = "Canonical"
